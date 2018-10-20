92e3191f00f36102b268980f225910e52bac41aa
Vesna <vesna.petrovska25@gmail.com>
2018-10-20 22:04:44 +0200

    recaptcha validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,9 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-        ]);
+            'g-recaptcha-response' => 'sometimes|required'
+            ],
+            ['g-recaptcha-response.*' => 'Please verify that you are not a robot']);
 
 
 
